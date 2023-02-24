7412087389662cb59fb4b1b73e42cb68620d519a
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-24 15:39:53 +0530

    day-3

---
+++ Js/Script.js
@@ -0,0 +1,7 @@
+window.onscroll = ()=>{
+    if(window.screenY > 80){
+        document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.add('active');
+    }
+}
