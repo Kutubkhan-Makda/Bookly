b58c82079cf5a95f408edc940eeac5f8df34a4ea
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-03-05 17:12:04 +0530

    day-6

--- Js/Script.js
+++ Js/Script.js
@@ -21,3 +21,13 @@ window.onload = ()=>{
         document.querySelector('.header .header-2').classList.remove('active');
     }
 }
+
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = ()=>{
+    loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = ()=>{
+    loginForm.classList.remove('active');
+}
