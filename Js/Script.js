c2787b203a10c085fe93037f53c8a75e5e1f8cf0
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-03-05 23:53:47 +0530

    day-6

--- Js/Script.js
+++ Js/Script.js
@@ -31,3 +31,19 @@ document.querySelector('#login-btn').onclick = ()=>{
 document.querySelector('#close-login-btn').onclick = ()=>{
     loginForm.classList.remove('active');
 }
+
+
+var swiper = new Swiper(".books-slider", {
+    loop:true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
