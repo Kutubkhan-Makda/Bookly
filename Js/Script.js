3b2e17a125fcd3391dbe2cb34e52b13a0de757a7
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-03-18 23:09:33 +0530

    day-14

--- Js/Script.js
+++ Js/Script.js
@@ -81,3 +81,25 @@ var swiper = new Swiper(".books-slider", {
       },
     },
   });
+
+
+  var swiper = new Swiper(".arrivals-slider", {
+    spaceBetween:10,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
